79ecf54a89c55320a370198709f05e525f93cbd9
Qinwen Yan <769423645@qq.com>
2026-02-12 15:04:57 +0800

    skip the exercise 47, 48

---
+++ Chapter13_copy_control/exercises/exercise_13_47-48.cpp
@@ -0,0 +1,16 @@
+/*
+* Exercise 13.47: 
+* Give the copy constructor and copy-assignment operator in
+* your String class from exercise 13.44 in § 13.5 (p. 531) a statement that
+* prints a message each time the function is executed.s
+*/
+
+
+/*
+* Exercise 13.48: 
+* Define a vector<String> and call push_back several
+* times on that vector. Run your program and see how often Strings are
+* copied.
+*/
+
+//Answer: 47，48题太大了不做了
