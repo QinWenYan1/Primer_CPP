6457c92a0fab27bf80f6c60636a954af35f77ade
Qinwen Yan <769423645@qq.com>
2026-03-12 13:54:08 +0800

    update the exercise

--- Chapter15_object_oriented_programming/exercises/exercise_3/Quote.h
+++ Chapter15_object_oriented_programming/exercises/exercise_3/Quote.h
@@ -2,6 +2,13 @@
 #define QUOTE_H
 
 #include <string>
+#include <iostream>
+
+/*
+* Exercise 15.3: 
+* Define your own versions of the Quote class and the
+* print_total function.
+*/
 
 class Quote {
 
@@ -9,12 +16,9 @@ public:
     Quote() = default; //默认构造函数
     Quote(const std::string &book, const double &sales_price):
         bookNo(book), price(sales_price) {} 
-    std::string isbn(){ return bookNo; }
-
+    std::string isbn() const { return bookNo; }
     virtual double net_price(std::size_t n ) const { return n*price; }
-    
     virtual ~Quote() = default; 
-    
     
 private:
     const std::string bookNo;
@@ -23,5 +27,16 @@ protected:
     double price; 
 
 }; 
+
+inline double &print_total(std::ostream &os, const Quote &item, const std::size_t &count){
+
+    double ret = item.net_price(count);
+    os << "ISBN: " << item.isbn() // calls Quote::isbn
+    << " # sold: " << count << " total due: " << ret << std::endl;
+    return ret;
+
+}
+ 
+
 
 #endif 
